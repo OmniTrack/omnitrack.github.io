456d9e746ad13e42b519e4387c5d33cd4b8a0deb
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-06-20 17:05:15 +0900

    arranged streamline features contents.

--- src/app/acknowledgement/acknowledgement.component.ts
+++ src/app/acknowledgement/acknowledgement.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcknowledgementComponent implements OnInit {
 
+  mockups : Array<string>
+
   constructor() { }
 
   ngOnInit() {
+
+    this.mockups = [
+      "http://www.titanui.com/49889-galaxy-s6-edge-in-hand-mockup-psd/",
+      "https://lstore.graphics/freebies/iphone_7/",
+      "https://www.mockupdeals.com/free-mockup-htc-android-brainstorming/",
+      "https://dribbble.com/shots/2433989-Android-Device-Mockups",
+      "http://www.72pxdesigns.com/template/google-pixel-psd-mockup-front/"
+    ]
   }
 
 }
