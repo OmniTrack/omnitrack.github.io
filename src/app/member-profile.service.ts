afaca5e91ba4ca5ccbba288f2bf0b20849d8c696
Young-Ho Kim <yghokim@umd.edu>
2021-10-08 07:20:01 -0400

    elaborated affiliation

--- src/app/member-profile.service.ts
+++ src/app/member-profile.service.ts
@@ -3,22 +3,25 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MemberProfileService {
 
-  readonly jhjeon ={
-          name: "Jae Ho Jeon",
-          title: "Software Engineer",
-          affiliation: "Google",
-          email: "jaehojeon (at) google.com",
-          web: "#",
-          thumb:"jhjeon_portrait.jpg"
-        }
+  readonly jhjeon = {
+    name: "Jae Ho Jeon",
+    title: "Software Engineer",
+    affiliation: "Google",
+    email: "jaehojeon (at) google.com",
+    web: "#",
+    thumb: "jhjeon_portrait.jpg",
+    sup: "1"
+  }
 
   readonly yhkim = {
-        name: "Young-Ho Kim",
-        title: "Postdoc",
-        affiliation: "University of Maryland",
-        email: "yghokim (at) umd.edu",
-        web: "http://younghokim.net",
-        thumb:"yhkim_portrait.jpg"
+    name: "Young-Ho Kim",
+    title: "Postdoc",
+    affiliation: "University of Maryland",
+    email: "yghokim (at) umd.edu",
+    web: "http://younghokim.net",
+    thumb: "yhkim_portrait.jpg",
+    original: "Seoul National University",
+    sup: "1"
   }
 
   readonly blee = {
@@ -27,7 +30,7 @@ export class MemberProfileService {
     affiliation: "Microsoft Research",
     email: "bongshin (at) microsoft.com",
     web: "http://bongshiny.com",
-    thumb:"author_bongshin.png"
+    thumb: "author_bongshin.png"
   }
 
   readonly echoe = {
@@ -35,8 +38,8 @@ export class MemberProfileService {
     title: "Associate Professor",
     affiliation: "University of Maryland",
     email: "choe (at) umd.edu",
-    web:"http://eunkyoungchoe.com",
-    thumb:"choe_portrait.png"
+    web: "http://eunkyoungchoe.com",
+    thumb: "choe_portrait.png"
   }
 
   readonly jseo = {
@@ -44,8 +47,8 @@ export class MemberProfileService {
     title: "Professor",
     affiliation: "Seoul Nat'l University",
     email: "jseo (at) snu.ac.kr",
-    web:"http://hcil.snu.ac.kr/people/jinwook-seo",
-    thumb:"jseo_portrait.jpg"
+    web: "http://hcil.snu.ac.kr/people/jinwook-seo",
+    thumb: "jseo_portrait.jpg"
   }
 
   readonly jhkim = {
@@ -53,7 +56,8 @@ export class MemberProfileService {
     title: "Software Engineer",
     email: "",
     affiliation: "NCSOFT",
-    thumb: "jhkim_portrait.jpg"
+    thumb: "jhkim_portrait.jpg",
+    sup: "1"
   }
 
   readonly valerie = {
@@ -61,26 +65,28 @@ export class MemberProfileService {
     title: "Master Student",
     email: "",
     affiliation: "KAIST",
-    thumb: "valerie_portrait.jpg"
+    thumb: "valerie_portrait.jpg",
+    original: "Universität Zürich",
+    sup: "1"
   }
 
   constructor() {
 
   }
 
-  public ubicomp2017Authors(): Array<any>{
+  public ubicomp2017Authors(): Array<any> {
     return [this.yhkim, this.jhjeon, this.blee, this.echoe, this.jseo]
   }
 
-  public pastMembers(): Array<any>{
+  public pastMembers(): Array<any> {
     return [this.jhjeon]
   }
 
-  public members(): Array<any>{
+  public members(): Array<any> {
     return [this.yhkim, this.blee, this.echoe, this.jseo]
   }
 
-  public contributors(): Array<any>{
+  public contributors(): Array<any> {
     return [this.jhkim, this.valerie]
   }
 
